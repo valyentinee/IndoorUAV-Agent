94fe57a273beeb56d36b12e9a242522cc1453d27
yuliu <yuliu_@hust.edu.cn>
2026-02-10 22:47:20 +0800

    Update vln_eval.sh

--- vln_eval.sh
+++ vln_eval.sh
@@ -5,6 +5,7 @@ gnome-terminal -- bash -c "conda activate uv && python online_eval/vln_eval/mode
 sleep 12
 
 gnome-terminal -- bash -c "conda activate habitat && python online_eval/vln_eval/sim_runner.py; exec bash"
+# if the sim_runner.py run into problem, running this command before might be helpful: Xvfb :99 -screen 0 1024x768x24 & export DISPLAY=:99
 
 sleep 1
 
